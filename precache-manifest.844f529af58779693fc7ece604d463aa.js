6b40f689c6cec18ec66b90291742f65f4eeca5c7
Armando <a.guerra58@hotmail.com>
2021-10-07 16:32:40 -0300

    Updates

---
+++ precache-manifest.844f529af58779693fc7ece604d463aa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da33e323c6532db8884ecd4a08ee6f1b",
+    "url": "/ProjectRecipeApp/index.html"
+  },
+  {
+    "revision": "56e785a381511d4c5b19",
+    "url": "/ProjectRecipeApp/static/css/main.0a3269c4.chunk.css"
+  },
+  {
+    "revision": "44ce2c42b86ee9df7c86",
+    "url": "/ProjectRecipeApp/static/js/2.a898d0ca.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/ProjectRecipeApp/static/js/2.a898d0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e785a381511d4c5b19",
+    "url": "/ProjectRecipeApp/static/js/main.39e0f712.chunk.js"
+  },
+  {
+    "revision": "53e1d6407f3f3a60ad6f",
+    "url": "/ProjectRecipeApp/static/js/runtime-main.c3b6c45d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/ProjectRecipeApp/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/ProjectRecipeApp/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/ProjectRecipeApp/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/ProjectRecipeApp/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/ProjectRecipeApp/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/ProjectRecipeApp/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/ProjectRecipeApp/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/ProjectRecipeApp/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
